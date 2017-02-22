e05bc03014eff16a82a713e59bc09c78eb05062c
John Skender <jnskender@gmail.com>
2017-02-22 14:31:38 -0600

    my_select passes

--- Enumerable/main.rb
+++ Enumerable/main.rb
@@ -11,12 +11,20 @@ module Enumerable
       for i in 0...length do
           yield self[i] , index(i)
       end
-      self
+       self
     end
 
     def my_select
-
+      values =[]
+      self.my_each {|x| values << x if yield(x)}
+      values
     end
+
+    def my_all
+      values = []
+      self.my_each {|x| values << false if !yield(x)}
+    end
+
 end
 
 array = [0,1,2,3]
@@ -25,10 +33,16 @@ puts '----------Each-----------'
 array.each { |x| puts x }
 puts "\n---------My_Each---------"
 array.my_each { |x| puts x }
-=end
-
 
 puts '----------Each_W/_Index-----------'
 array.each_with_index { |v,i| puts "#{v} #{i}" }
 puts '----------My_Each_W/_Index-----------'
 array.my_each_with_index { |v,i| puts "#{v} #{i}" }
+
+puts '----------Select-----------'
+array.select { |x| puts x if x.even?  }   #=> [2, 4]
+puts '----------My_Select-----------'
+array.my_select { |x| puts x if x.even?  }   #=> [2, 4]
+=end
+puts '----------Any?-----------'
+%w[any bear cat].any? { |x| puts x.length >= 4 }   #=> [true]
