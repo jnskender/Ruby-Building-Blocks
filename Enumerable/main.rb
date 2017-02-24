3ff40ea64988c8a666c0b1c9909f3d8c9acb94fe
John Skender <jnskender@gmail.com>
2017-02-23 22:06:35 -0600

    All methods pass

--- Enumerable/main.rb
+++ Enumerable/main.rb
@@ -15,9 +15,9 @@ module Enumerable
     end
 
     def my_select
-      values =[]
-      self.my_each {|x| values << x if yield x}
-      values
+      result =[]
+      self.my_each {|x| result << x if yield x}
+      result
     end
 
     def my_all?
@@ -25,27 +25,41 @@ module Enumerable
       true
     end
 
+    def my_any?
+      self.my_each {|x| return true if yield x}
+      false
+    end
+
+    def my_none?
+      self.my_each{|x| return false unless !yield x}
+      true
+    end
+
+    def my_count
+      count = 0
+      if block_given?
+        my_each {|x| count += 1 if yield x}
+      else
+        length.times {count +=1}
+      end
+      count
+    end
 end
 
-=begin
-array = [0,1,2,3]
-puts '----------Each-----------'
-array.each { |x| puts x }
-puts "\n---------My_Each---------"
-array.my_each { |x| puts x }
+  def my_map(mapper = nil)
+    result = []
+    my_each do |x|
+      if !mapper.nil?
+        result << x if mapper.call(x)
+      else
+        result << x if yield x
+      end
+    end
+    result
+  end
 
-puts '----------Each_W/_Index-----------'
-array.each_with_index { |v,i| puts "#{v} #{i}" }
-puts '----------My_Each_W/_Index-----------'
-array.my_each_with_index { |v,i| puts "#{v} #{i}" }
-
-puts '----------Select-----------'
-array.select { |x| puts x if x.even?  }   #=> [2, 4]
-puts '----------My_Select-----------'
-array.my_select { |x| puts x if x.even?  }   #=> [2, 4]
-=end
-array = ["1234","1234"]
-puts '----------All?-----------'
-puts array.all? { |x| x.length >= 4 }   #=> [true]
-puts '----------My_All?-----------'
-puts array.my_all? { |x| x.length >= 4 }   #=> [true]
+  def my_inject(carry_value = nil)
+    carry_value = shift if carry_value.nil?
+    my_each { |x| carry_value = yield(carry_value, x) }
+    carry_value
+  end
