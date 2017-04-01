4bd2f7ced6950f428bf5de535ac8dd45d733b5ee
John Skender <jnskender@gmail.com>
2017-04-01 12:33:12 -0500

    Add testing for my_map

--- Enumerable/spec/enumberable_spec.rb
+++ Enumerable/spec/enumberable_spec.rb
@@ -40,4 +40,22 @@ describe Enumerable do
             end
         end
     end # End my select
+
+    describe '#my_map' do
+        context 'Without Block' do
+            it 'returns an enumerator' do
+                expect(array.my_map).to be_instance_of Enumerator
+            end
+        end
+
+        context 'With Block' do
+            it 'Returns array that satisfies the block conditions ' do
+                expect(array.map{|a| 2 * a}).to eql [2,4,6,8,10]
+            end
+
+            it 'returns empty array when given an empty array' do
+                expect(empty_array.map(&:even?)).to eql []
+            end
+        end
+    end # End my select
 end
