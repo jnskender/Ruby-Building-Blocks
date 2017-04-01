8872526a36a393c121f0c8cdae5f7db18f554431
John Skender <jnskender@gmail.com>
2017-04-01 12:32:47 -0500

    remove use of self

--- Enumerable/lib/enumerable.rb
+++ Enumerable/lib/enumerable.rb
@@ -2,70 +2,71 @@
 module Enumerable
     def my_each
         if block_given?
-          for i in 0...length do
-              yield self.to_a[i]
-          end
+            for i in 0...length do
+                yield to_a[i]
+            end
         else
-          return self.to_enum
+            return to_enum
         end
         self
     end
 
     def my_each_with_index
-      return self.to_enum unless block_given?
-      for i in 0...length do
-          yield self[i] , index(i)
-      end
-       self
+        return to_enum unless block_given?
+        for i in 0...length do
+            yield self[i], index(i)
+        end
+        self
     end
 
     def my_select
-      return self.to_enum unless block_given?
-      result =[]
-      self.my_each {|x| result << x if yield x}
-      result
+        return to_enum unless block_given?
+        result = []
+        my_each { |x| result << x if yield x }
+        result
     end
 
     def my_all?
-      self.my_each {|x| return false unless yield x}
-      true
+        my_each { |x| return false unless yield x }
+        true
     end
 
     def my_any?
-      self.my_each {|x| return true if yield x}
-      false
+        my_each { |x| return true if yield x }
+        false
     end
 
     def my_none?
-      self.my_each{|x| return false unless !yield x}
-      true
+        my_each { |x| return false if yield x }
+        true
     end
 
     def my_count
-      count = 0
-      if block_given?
-        my_each {|x| count += 1 if yield x}
-      else
-        length.times {count +=1}
-      end
-      count
+        count = 0
+        if block_given?
+            my_each { |x| count += 1 if yield x }
+        else
+            length.times { count += 1 }
+        end
+        count
+    end
+
+    def my_map(mapper = nil)
+        result = []
+        return to_enum unless block_given?
+        my_each do |x|
+            if !mapper.nil?
+                result << x if mapper.call(x)
+            else
+                result << x if yield x
+            end
+        end
+        result
+    end
+
+    def my_inject(carry_value = nil)
+        carry_value = shift if carry_value.nil?
+        my_each { |x| carry_value = yield(carry_value, x) }
+        carry_value
     end
 end
-
-  def my_map(mapper = nil)
-    result = []
-    my_each do |x|
-      if !mapper.nil?
-        result << x if mapper.call(x)
-      else
-        result << x if yield x
-      end
-    end
-    result
-  end
-
-  def my_inject(carry_value = nil)
-    carry_value = shift if carry_value.nil?
-    my_each { |x| carry_value = yield(carry_value, x) }
-    carry_value
-  end
